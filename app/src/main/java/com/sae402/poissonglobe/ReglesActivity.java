de2759d2788ee299cbe08890a995ea1a6c6c02bb
Lindsay Chouteau <i2400974@cri.univ-lemans.fr>
2026-05-19 16:30:28 +0200

    vue regles & back bouton regles du jeu

---
+++ app/src/main/java/com/sae402/poissonglobe/ReglesActivity.java
@@ -0,0 +1,23 @@
+package com.sae402.poissonglobe;
+
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReglesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.regles);
+        View btnRetour = findViewById(R.id.btnRetour);
+
+        btnRetour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
